156cf6e67141be2c63ae18c241fa6ac0cfc50a1d
Gerard Dunn <gerard.dunn.92@gmail.com>
2025-05-02 18:06:43 +1200

    setting up projects page

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,6 +9,20 @@ export default function Projects(): JSX.Element {
           <h1 className="text-4xl font-bold text-center text-white">
             Geared Up
           </h1>
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <p>
+            This website was our final group project during my time at Dev
+            Academy. For this challenge we put all of our combined knowledge and
+            skills to the test to produce a high quality product that
+            accomplished the MVP we had set out to achieve and more. This
+            website was designed to be a companion tool for the Great Walks of
+            NZ, preparing the user for any walk they choose to attempt by
+            providing them with a list of equipment required by that walk. The
+            user is able to keep track of which walks they are planning or
+            completed as well as the equipment they own.{' '}
+          </p>
         </div>
         <div>
           <h1 className="text-4xl font-bold text-center text-white">
